864cf0481d54c3790f14d5b684b76af794465d05
Dongmin Kim <ddmkim94@gmail.com>
2024-04-09 14:20:13 +0900

    [Silver I] Title: 단지번호붙이기, Time: 124 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,20 +1,21 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
-// 단지 번호 붙이기
+// 단지번호붙이기
 public class Main {
 
-    static Queue<int[]> queue = new LinkedList<>();
-    // 상좌하우
-    static int[] dx = {-1 , 0, 1, 0};
-    static int[] dy = {0, -1, 0, 1};
+    // 상하좌우
     static int n;
-    static int houseNum = 0; // 단지 번호
-    static Map<Integer, Integer> house = new HashMap<>();
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
     static int[][] map;
     static boolean[][] visited;
+    static PriorityQueue<Integer> pq = new PriorityQueue<>();
+    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -22,47 +23,40 @@ public class Main {
 
         map = new int[n][n];
         visited = new boolean[n][n];
-
         for (int i = 0; i < n; i++) {
             String str = br.readLine();
-            for (int j = 0; j < str.length(); j++) {
+            for (int j = 0; j < n; j++) {
                 map[i][j] = str.charAt(j) - 48;
             }
         }
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (!visited[i][j] && map[i][j] == 1) {
-                    int cnt = bfs(i, j); // 각 단지별 집의 개수
-                    house.put(houseNum, cnt);
+                if(!visited[i][j] && map[i][j] == 1) {
+                    bfs(i, j);
                 }
             }
         }
-        System.out.println(result());
-    }
 
-    private static String result() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(house.size()).append("\n");
-
-        List<Integer> list = new ArrayList<>(house.values());
-        Collections.sort(list);
-        for (Integer l : list) {
-            sb.append(l).append("\n");
+        sb.append(pq.size()).append("\n");
+        while (!pq.isEmpty()) {
+            sb.append(pq.poll()).append("\n");
         }
 
-        return sb.toString();
+        System.out.println(sb);
+
     }
 
-    private static int bfs(int x, int y) {
-        int cnt = 1;
-        queue.offer(new int[]{x, y});
+    private static void bfs(int x, int y) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{x, y});
         visited[x][y] = true;
+        int count = 1;
 
         while (!queue.isEmpty()) {
-            int[] cur = queue.poll();
-            int curX = cur[0];
-            int curY = cur[1];
+            int[] pos = queue.poll();
+            int curX = pos[0];
+            int curY = pos[1];
 
             for (int i = 0; i < 4; i++) {
                 int nx = curX + dx[i];
@@ -70,14 +64,14 @@ public class Main {
 
                 if (nx >= 0 && nx < n && ny >= 0 && ny < n) {
                     if (!visited[nx][ny] && map[nx][ny] == 1) {
-                        cnt++;
                         visited[nx][ny] = true;
-                        queue.offer(new int[]{nx, ny});
+                        queue.add(new int[]{nx, ny});
+                        count++;
                     }
                 }
             }
         }
-        houseNum++;
-        return cnt;
+
+        pq.add(count);
     }
 }
