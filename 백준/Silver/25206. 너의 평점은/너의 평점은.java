03b51be6ad40903069770b67ca9648d455facfcc
Dongmin Kim <ddmkim94@gmail.com>
2024-06-10 16:06:47 +0900

    [Silver V] Title: 너의 평점은, Time: 124 ms, Memory: 14324 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.java
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.java
@@ -9,8 +9,8 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        double total = 0; // 학점 * 과목 평점
-        double totalScore = 0; // 학점의 총합
+        double total = 0;
+        double totalScore = 0;
 
         for (int i = 0; i < 20; i++) {
             st = new StringTokenizer(br.readLine());
@@ -18,49 +18,42 @@ public class Main {
             String subject = st.nextToken();
             double score = Double.parseDouble(st.nextToken());
             String grade = st.nextToken();
+            
+            if (!grade.equals("P")) {
+                totalScore += score;
+            }
 
             switch (grade) {
                 case "A+":
                     total += score * 4.5;
-                    totalScore += score;
                     break;
                 case "A0":
                     total += score * 4.0;
-                    totalScore += score;
                     break;
                 case "B+":
                     total += score * 3.5;
-                    totalScore += score;
                     break;
                 case "B0":
                     total += score * 3.0;
-                    totalScore += score;
                     break;
                 case "C+":
                     total += score * 2.5;
-                    totalScore += score;
                     break;
                 case "C0":
                     total += score * 2.0;
-                    totalScore += score;
                     break;
                 case "D+":
                     total += score * 1.5;
-                    totalScore += score;
                     break;
                 case "D0":
                     total += score;
-                    totalScore += score;
                     break;
                 case "F":
                     total += score * 0.0;
-                    totalScore += score;
-                    break;
-                default:
                     break;
             }
         }
-        
+
         System.out.println(total / totalScore);
     }
 }
