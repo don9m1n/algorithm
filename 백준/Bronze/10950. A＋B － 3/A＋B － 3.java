bda0999e2732c1e337ecbe1134ec77d2856cf709
Dongmin Kim <ddmkim94@gmail.com>
2024-07-28 20:09:06 +0900

    [Bronze V] Title: A+B - 3, Time: 132 ms, Memory: 14136 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        for (int i = 0; i < t; i++) {
+            st = new StringTokenizer(br.readLine());
+            int num1 = Integer.parseInt(st.nextToken());
+            int num2 = Integer.parseInt(st.nextToken());
+
+            System.out.println(num1 + num2);
+        }
+    }
+}
