2af536bdcd6e43171d3186d2f2de6d08556319d8
Dongmin Kim <ddmkim94@gmail.com>
2024-07-28 20:19:57 +0900

    [Bronze V] Title: A+B - 5, Time: 132 ms, Memory: 14136 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // StringTokenizer EOF 불가!
+        StringTokenizer st;
+        while(true) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int num1 = Integer.parseInt(st.nextToken());
+            int num2 = Integer.parseInt(st.nextToken());
+
+            if (num1 == 0 && num2 == 0) {
+                break;
+            }
+
+            System.out.println(num1 + num2);
+        }
+    }
+}
