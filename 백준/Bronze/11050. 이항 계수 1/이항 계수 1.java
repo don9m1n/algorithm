6842d50a3e7d37573d471c6fabeac235ffaebbd0
dongmin <ddmkim94@gmail.com>
2024-07-30 11:57:42 +0900

    [Bronze I] Title: 이항 계수 1, Time: 124 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -10,23 +10,17 @@ public class Main {
 
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-        
+
         // 이항계수 공식: n! / (n - k)! * k!
         System.out.println(factorial(n) / (factorial(n - k) * factorial(k)));
     }
 
     private static int factorial(int n) {
 
-        if (n == 0) {
-            return 1;
-        }
-
-        if (n == 1) {
+        if (n == 0 || n == 1) {
             return 1;
         }
 
         return n * factorial(n - 1);
     }
-
-
 }
