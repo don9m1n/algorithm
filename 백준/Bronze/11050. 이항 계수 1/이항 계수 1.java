ca70eca189cd157661afb04901b2994a925b005f
dongmin <ddmkim94@gmail.com>
2024-07-30 11:57:02 +0900

    [Bronze I] Title: 이항 계수 1, Time: 124 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        
+        // 이항계수 공식: n! / (n - k)! * k!
+        System.out.println(factorial(n) / (factorial(n - k) * factorial(k)));
+    }
+
+    private static int factorial(int n) {
+
+        if (n == 0) {
+            return 1;
+        }
+
+        if (n == 1) {
+            return 1;
+        }
+
+        return n * factorial(n - 1);
+    }
+
+
+}
