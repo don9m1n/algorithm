c3e1dcc8e6f70ef3cb96c39b07976396f2e2a174
dongmin <ddmkim94@gmail.com>
2024-08-02 19:53:21 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 124 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -12,6 +12,12 @@ public class Main {
         int b = Integer.parseInt(st.nextToken());
         int v = Integer.parseInt(st.nextToken());
 
+        /**
+         * (막대 높이 - 낮에 올라가는 높이) 이상까지 올라오면 다음날 정상에 도착이 가능함.
+         * 하루에 올라갈 수 있는 높이는 (올라가는 높이 - 미끄러지는 높이)
+         * 올림((막대 높이 - 낮에 올라가는 높이) / 하루에 올라갈 수 있는 높이) + 1
+         */
+
         System.out.println((int) Math.ceil((double) (v - a) / (a - b)) + 1);
     }
 }
