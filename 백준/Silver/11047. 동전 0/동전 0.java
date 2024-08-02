aafdc1dbc300e5e00d7258cfad88edbf9b20b27a
dongmin <ddmkim94@gmail.com>
2024-08-02 19:26:58 +0900

    [Silver IV] Title: 동전 0, Time: 188 ms, Memory: 15980 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] coin = new int[n];
+        for (int i = 0; i < n; i++) {
+            coin[i] = Integer.parseInt(br.readLine());
+        }
+
+        int count = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            while (k >= coin[i]) {
+                k -= coin[i];
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
