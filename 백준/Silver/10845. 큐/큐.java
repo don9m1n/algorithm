5335aa7aeb3c7e975f891e28062a42183d7e3e8a
dongmin <ddmkim94@gmail.com>
2024-08-03 22:27:21 +0900

    [Silver IV] Title: 큐, Time: 160 ms, Memory: 19108 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.java
+++ 백준/Silver/10845. 큐/큐.java
@@ -6,45 +6,57 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		Queue<Integer> que = new LinkedList<Integer>();
-		int last = 0;
-		
-		for(int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			String S = st.nextToken();
-			
-			switch(S) {
-			case "push" :
-				last = Integer.parseInt(st.nextToken());
-				que.offer(last);
-				break;
-			case "pop" :
-				if(que.isEmpty()) sb.append(-1).append("\n");
-				else sb.append(que.poll()).append("\n");
-				break;
-			case "size" :
-				sb.append(que.size()).append("\n");
-				break;
-			case "empty" :
-				if(que.isEmpty()) sb.append(1).append("\n");
-				else sb.append(0).append("\n");
-				break;
-			case "front" :
-				if(que.isEmpty()) sb.append(-1).append("\n");
-				else sb.append(que.peek()).append("\n");
-				break;
-			case "back" :
-				if(que.isEmpty()) sb.append(-1).append("\n");
-				else sb.append(last).append("\n");
-				break;
-			}
-		}
-		System.out.println(sb);
-	}
-	
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        Queue<Integer> queue = new LinkedList<>();
+        StringBuilder sb = new StringBuilder();
+        int last = 0;
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String command = st.nextToken();
+
+            switch (command) {
+                case "push":
+                    last = Integer.parseInt(st.nextToken());
+                    queue.add(last);
+                    break;
+                case "pop":
+                    if (queue.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(queue.poll()).append("\n");
+                    }
+                    break;
+                case "size":
+                    sb.append(queue.size()).append("\n");
+                    break;
+                case "empty":
+                    if (queue.isEmpty()) {
+                        sb.append(1).append("\n");
+                    } else {
+                        sb.append(0).append("\n");
+                    }
+                    break;
+                case "front":
+                    if (queue.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(queue.peek()).append("\n");
+                    }
+                    break;
+                case "back":
+                    if (queue.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(last).append("\n");
+                    }
+                    break;
+            }
+        }
+
+        System.out.println(sb);
+    }
 }
