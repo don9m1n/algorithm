131cbc92461179700d1250aa347b1f2c5ff407e9
dongmin <ddmkim94@gmail.com>
2024-08-03 22:25:38 +0900

    [Silver IV] Title: 큐, Time: 176 ms, Memory: 19900 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.java
+++ 백준/Silver/10845. 큐/큐.java
@@ -1,75 +1,50 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
-// 큐
 public class Main {
-
-    static StringBuilder sb;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-
-        sb = new StringBuilder();
-        Queue<Integer> queue = new LinkedList<>();
-        
-        StringTokenizer st;
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            String command = st.nextToken();
-            switch (command) {
-                case "push":
-                    queue.offer(Integer.parseInt(st.nextToken()));
-                    break;
-                case "pop":
-                    sb.append(pop(queue)).append("\n");
-                    break;
-                case "size":
-                    sb.append(queue.size()).append("\n");
-                    break;
-                case "empty":
-                    sb.append(empty(queue)).append("\n");
-                    break;
-                case "front":
-                    sb.append(front(queue)).append("\n");
-                    break;
-                case "back":
-                    queue = back(queue);
-                    break;
-            }
-        }
-
-        String result = sb.toString();
-        System.out.println(result);
-    }
-
-    private static Queue<Integer> back(Queue<Integer> queue) {
-        if (queue.isEmpty()) {
-            sb.append(-1).append("\n");
-        } else {
-            List<Integer> list = new ArrayList<>();
-            Queue<Integer> copy = new LinkedList<>();
-            while (!queue.isEmpty()) {
-                copy.offer(queue.peek());
-                list.add(queue.poll());
-            }
-            sb.append(list.get(list.size() - 1)).append("\n");
-            queue = copy;
-        }
-        return queue;
-    }
-
-    private static int front(Queue<Integer> queue) {
-        return queue.isEmpty() ? -1 : queue.peek();
-    }
-
-    private static int empty(Queue<Integer> queue) {
-        return queue.isEmpty() ? 1 : 0;
-    }
-
-    private static int pop(Queue<Integer> queue) {
-        return queue.isEmpty() ? -1 : queue.poll();
-    }
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		Queue<Integer> que = new LinkedList<Integer>();
+		int last = 0;
+		
+		for(int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			String S = st.nextToken();
+			
+			switch(S) {
+			case "push" :
+				last = Integer.parseInt(st.nextToken());
+				que.offer(last);
+				break;
+			case "pop" :
+				if(que.isEmpty()) sb.append(-1).append("\n");
+				else sb.append(que.poll()).append("\n");
+				break;
+			case "size" :
+				sb.append(que.size()).append("\n");
+				break;
+			case "empty" :
+				if(que.isEmpty()) sb.append(1).append("\n");
+				else sb.append(0).append("\n");
+				break;
+			case "front" :
+				if(que.isEmpty()) sb.append(-1).append("\n");
+				else sb.append(que.peek()).append("\n");
+				break;
+			case "back" :
+				if(que.isEmpty()) sb.append(-1).append("\n");
+				else sb.append(last).append("\n");
+				break;
+			}
+		}
+		System.out.println(sb);
+	}
+	
 }
