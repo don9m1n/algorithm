a77d5260c11cc694becd87010b624aedb78a16e7
dongmin <ddmkim94@gmail.com>
2024-08-04 18:18:04 +0900

    [Bronze V] Title: 개, Time: 104 ms, Memory: 13940 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("|\\_/|").append("\n");
+        sb.append("|q p|   /}").append("\n");
+        sb.append("( 0 )\"\"\"\\").append("\n");
+        sb.append("|\"^\"`    |").append("\n");
+        sb.append("||_/=\\\\__|").append("\n");
+
+        System.out.println(sb);
+    }
+}
