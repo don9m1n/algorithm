b499303c530e6d5885d5218d280c9fa2f6612abe
dongmin <ddmkim94@gmail.com>
2024-08-04 18:16:53 +0900

    [Bronze V] Title: 고양이, Time: 92 ms, Memory: 14124 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        
+        sb.append("\\    /\\").append("\n");
+        sb.append(" )  ( ')").append("\n");
+        sb.append("(  /  )").append("\n");
+        sb.append(" \\(__)|").append("\n");
+
+        System.out.println(sb);
+    }
+}
