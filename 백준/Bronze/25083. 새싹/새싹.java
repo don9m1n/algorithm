ab1c3a6c8b3df3b321a918e0bb5d59c6b62c85ee
dongmin <ddmkim94@gmail.com>
2024-08-04 18:22:34 +0900

    [Bronze V] Title: 새싹, Time: 100 ms, Memory: 13920 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("         ,r'\"7").append("\n");
+        sb.append("r`-_   ,'  ,/").append("\n");
+        sb.append(" \\. \". L_r'").append("\n");
+        sb.append("   `~\\/").append("\n");
+        sb.append("      |").append("\n");
+        sb.append("      |").append("\n");
+
+        System.out.println(sb);
+    }
+}
