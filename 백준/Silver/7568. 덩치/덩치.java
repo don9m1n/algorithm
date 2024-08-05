9c3ca2efecc06f742953043cbbfb3e880f57a944
dongmin <ddmkim94@gmail.com>
2024-08-05 15:53:51 +0900

    [Silver V] Title: 덩치, Time: 104 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.java
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -15,21 +15,20 @@ public class Main {
             arr[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        int[] answer = new int[n];
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) {
             int k = 1;
             for (int j = 0; j < n; j++) {
-                if (i != j && (arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1])) {
+                if(i == j) {
+                    continue;
+                }
+
+                if (arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) {
                     k++;
                 }
             }
 
-            answer[i] = k;
-        }
-
-        StringBuilder sb = new StringBuilder();
-        for (int a : answer) {
-            sb.append(a).append(" ");
+            sb.append(k).append(" ");
         }
 
         System.out.println(sb);
