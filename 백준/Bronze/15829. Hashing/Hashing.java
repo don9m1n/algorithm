531efef72e7d07dff7cdb1b24ec8d4e21b1577dd
dongmin <ddmkim94@gmail.com>
2024-08-05 13:27:58 +0900

    [Bronze II] Title: Hashing, Time: 104 ms, Memory: 14408 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -9,11 +10,11 @@ public class Main {
         int l = Integer.parseInt(br.readLine());
         String word = br.readLine();
 
-        double sum = 0;
+        BigInteger result = BigInteger.ZERO;
         for (int i = 0; i < l; i++) {
-            sum += (word.charAt(i) - 96) * Math.pow(31, i);
+            result = result.add(BigInteger.valueOf(word.charAt(i) - 96).multiply(BigInteger.valueOf(31).pow(i)));
         }
 
-        System.out.println((int) (sum % 1234567891));
+        System.out.println(result.mod(BigInteger.valueOf(1234567891)));
     }
 }
