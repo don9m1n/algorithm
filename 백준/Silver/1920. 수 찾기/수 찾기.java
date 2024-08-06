b57d67f3594126f766f93ea2f168b9889b6af95a
dongmin <ddmkim94@gmail.com>
2024-08-06 16:40:27 +0900

    [Silver IV] Title: 수 찾기, Time: 640 ms, Memory: 58968 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,33 +1,31 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        Set<Integer> set = new HashSet<>();
+        Map<Integer, Integer> map = new LinkedHashMap<>();
 
-        Map<Long, Integer> map1 = new LinkedHashMap<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            map1.put(Long.parseLong(st.nextToken()), i);
+            set.add(Integer.parseInt(st.nextToken()));
         }
 
         int m = Integer.parseInt(br.readLine());
+        
         st = new StringTokenizer(br.readLine());
-
-        Map<Integer, Long> map2 = new LinkedHashMap<>();
         for (int i = 0; i < m; i++) {
-            map2.put(i, Long.parseLong(st.nextToken()));
+            map.put(i, Integer.parseInt(st.nextToken()));
         }
 
         StringBuilder answer = new StringBuilder();
-        for (Integer key : map2.keySet()) {
-            answer.append(map1.containsKey(map2.get(key)) ? 1 : 0).append("\n");
+        for (Integer key : map.keySet()) {
+            answer.append(set.contains(map.get(key)) ? 1 : 0).append("\n");
         }
 
         System.out.println(answer);
