660cf99e5e255575749c37ce4bcdadb9f97959bd
dongmin <ddmkim94@gmail.com>
2024-08-07 13:16:16 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 216 ms, Memory: 19232 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder answer = new StringBuilder();
+
+        String sentence = "";
+        while (!(sentence = br.readLine()).equals(".")) {
+            answer.append(isBracket(sentence)).append("\n");
+        }
+
+        System.out.println(answer);
+    }
+
+    private static String isBracket(String sentence) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < sentence.length(); i++) {
+            char ch = sentence.charAt(i);
+
+            if (ch == '(' || ch == '[') {
+                stack.push(ch);
+            } else if (stack.isEmpty() && (ch == ')' || ch == ']')) {
+                return "no";
+            } else if (ch == ')') {
+                if (stack.peek() == '(') {
+                    stack.pop();
+                } else {
+                    stack.push(ch);
+                }
+            } else if (ch == ']') {
+                if (stack.peek() == '[') {
+                    stack.pop();
+                } else {
+                    stack.push(ch);
+                }
+            }
+        }
+
+        return stack.isEmpty() ? "yes" : "no";
+    }
+}
