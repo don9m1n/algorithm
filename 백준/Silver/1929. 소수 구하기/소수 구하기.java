ddb59509f698ce037495bd9931ef01c095d2ce3a
dongmin <ddmkim94@gmail.com>
2024-08-09 17:49:38 +0900

    [Silver III] Title: 소수 구하기, Time: 312 ms, Memory: 21736 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -11,22 +12,22 @@ public class Main {
         int m = Integer.parseInt(st.nextToken());
         int n = Integer.parseInt(st.nextToken());
 
-        StringBuilder answer = new StringBuilder();
-        for (int i = m; i <= n; i++) {
+        boolean[] isPrime = new boolean[n + 1];
+        Arrays.fill(isPrime, true);
 
-            if (i == 1) {
-                continue;
-            }
+        isPrime[0] = isPrime[1] = false;
 
-            boolean flag = false;
-            for (int j = 2; j <= Math.sqrt(i); j++) {
-                if (i % j == 0) {
-                    flag = true;
-                    break;
+        for (int i = 2; i * i <= n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= n; j += i) {
+                    isPrime[j] = false;
                 }
             }
+        }
 
-            if (!flag) {
+        StringBuilder answer = new StringBuilder();
+        for (int i = m; i <= n; i++) {
+            if (isPrime[i]) {
                 answer.append(i).append("\n");
             }
         }
