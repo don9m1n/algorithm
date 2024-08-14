5762a20c7766201dde64cfc9a0c8658c3a928e29
dongmin <ddmkim94@gmail.com>
2024-08-14 21:43:57 +0900

    [Bronze V] Title: CASIO, Time: 100 ms, Memory: 14072 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long n = Long.parseLong(st.nextToken());
+        long m = Long.parseLong(st.nextToken());
+
+        if (n == m) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
