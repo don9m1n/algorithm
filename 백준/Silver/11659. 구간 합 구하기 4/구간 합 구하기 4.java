4654dba7fa4c7b15720d8c16469fe03b17a5d541
dongmin <ddmkim94@gmail.com>
2024-08-19 19:54:50 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 564 ms, Memory: 54532 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n + 1];
+        int[] prefix = new int[n + 1];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            prefix[i] = prefix[i - 1] + arr[i];
+        }
+
+        StringBuilder answer = new StringBuilder();
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            if (x == y) {
+                answer.append(arr[y]).append("\n");
+            } else {
+                answer.append(prefix[y] - prefix[x - 1]).append("\n");
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
