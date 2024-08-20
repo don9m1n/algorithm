d7682275d59549271a59e320ea25dc4963de2a13
dongmin <ddmkim94@gmail.com>
2024-08-20 14:17:41 +0900

    [Silver I] Title: 회의실 배정, Time: 464 ms, Memory: 44608 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, (o1, o2) -> {
+            if (o1[1] == o2[1]) {
+                return o1[0] - o2[0];
+            }
+
+            return o1[1] - o2[1];
+        });
+
+        int count = 1;
+        int s = arr[0][0];
+        int e = arr[0][1];
+
+        for (int i = 1; i < n; i++) {
+
+            if (arr[i][0] >= e) {
+                count++;
+                e = arr[i][1];
+            }
+        }
+
+        System.out.println(count);
+    }
+}
