f7c9082e6c53cdef88da22b626baacc7db84fae0
dongmin <ddmkim94@gmail.com>
2024-08-22 17:32:35 +0900

    [Silver II] Title: 최소 힙, Time: 284 ms, Memory: 26380 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder answer = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        for (int i = 0; i < n; i++) {
+            int x = Integer.parseInt(br.readLine());
+
+            if (x > 0) {
+                pq.add(x);
+            } else {
+                answer.append(pq.isEmpty() ? 0 : pq.poll()).append("\n");
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
