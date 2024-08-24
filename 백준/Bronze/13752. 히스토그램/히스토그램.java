6b91af265e9175a0f6144211c3ba1cfee32b6aa1
dongmin <ddmkim94@gmail.com>
2024-08-24 20:11:07 +0900

    [Bronze IV] Title: 히스토그램, Time: 104 ms, Memory: 14312 KB -BaekjoonHub

---
+++ 백준/Bronze/13752. 히스토그램/히스토그램.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        StringBuilder answer = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            int k = Integer.parseInt(br.readLine());
+
+            answer.append("=".repeat(k));
+            answer.append("\n");
+        }
+
+        System.out.println(answer);
+    }
+}
