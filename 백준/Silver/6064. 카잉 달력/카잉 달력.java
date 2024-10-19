f49df8580a60a24be54f0f6c3af20cf99f153308
dongmin <ddmkim94@gmail.com>
2024-10-19 20:03:26 +0900

    [Silver I] Title: 카잉 달력, Time: 240 ms, Memory: 18412 KB -BaekjoonHub

--- 백준/Silver/6064. 카잉 달력/카잉 달력.java
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.java
@@ -3,43 +3,32 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-// 카잉 달력
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
 
-        StringTokenizer st;
         for (int i = 0; i < t; i++) {
-            st = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            boolean flag = false;
             int m = Integer.parseInt(st.nextToken());
             int n = Integer.parseInt(st.nextToken());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken()) - 1;
+            int y = Integer.parseInt(st.nextToken()) - 1;
 
-            int lcm = (m * n) / gcd(m, n);
-
-            x--;
-            y--;
-            int year = x;
-            while (year <= lcm) {
-                if (year % n == y) {
-                    System.out.println(year + 1);
+            for (int j = x; j < (n * m); j += m) {
+                if (j % n == y) {
+                    System.out.println(j + 1);
+                    flag = true;
                     break;
                 }
-                year += m;
             }
 
-            if (year > lcm) {
+            if (!flag) {
                 System.out.println(-1);
+
             }
         }
-    }
-
-    private static int gcd(int a, int b) {
-        if (a % b == 0) {
-            return b;
-        }
-        return gcd(b, a % b);
     }
 }
