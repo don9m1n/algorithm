d9c51086f1127805fa7a859a0c2dffaa432f9ca8
dongmin <ddmkim94@gmail.com>
2024-10-24 15:03:44 +0900

    [Silver I] Title: IOIOI, Time: 108 ms, Memory: 14268 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        String str = br.readLine();
+
+        // 부분 문자열
+        int i = 0;
+        int count = 0;
+        int answer = 0;
+        while (i < m - 1) {
+            if(str.charAt(i) == 'O' && str.charAt(i + 1) == 'I') {
+                count++;
+                if (count == n) {
+                    if (str.charAt(i - (count * 2 - 1)) == 'I') {
+                        answer++;
+                    }
+
+                    count--;
+                }
+
+                i += 2;
+            } else {
+                count = 0;
+                i++;
+            }
+        }
+        System.out.println(answer);
+    }
+}
