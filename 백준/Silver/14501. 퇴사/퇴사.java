5b83105aaa89f54d59b6bd71309d08308b306ea8
dongmin <ddmkim94@gmail.com>
2024-11-22 18:32:01 +0900

    [Silver III] Title: 퇴사, Time: 104 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -7,29 +7,30 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
-        int[][] arr = new int[n][2];
 
+        int[][] counsel = new int[n][2];
         StringTokenizer st;
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-
-            arr[i][0] = Integer.parseInt(st.nextToken());
-            arr[i][1] = Integer.parseInt(st.nextToken());
+            counsel[i][0] = Integer.parseInt(st.nextToken());
+            counsel[i][1] = Integer.parseInt(st.nextToken());
         }
 
         int[] dp = new int[n + 1];
-        for (int i = n - 1; i >= 0; i--) {
 
-            if (i + arr[i][0] > n) {
-                dp[i] = dp[i + 1];
+        for (int i = n - 1; i >= 0; i--) {
+            int t = counsel[i][0]; // 상담에 걸리는 시간
+            int p = counsel[i][1]; // 상담을 했을 때 얻는 수익
+
+            if (i + t <= n) {
+                dp[i] = Math.max(dp[i + 1], dp[i + t] + p);
             } else {
-                dp[i] = Math.max(dp[i + 1], dp[i + arr[i][0]] + arr[i][1]);
+                dp[i] = Math.max(dp[i], dp[i + 1]);
             }
         }
 
         System.out.println(dp[0]);
-    }
 
+    }
 }
