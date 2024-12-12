0c3c9cca729f468efd0ecfd26fa00c653488ae4a
dongmin <ddmkim94@gmail.com>
2024-12-12 19:39:46 +0900

    [level 2] Title: 올바른 괄호, Time: 21.68 ms, Memory: 55.3 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,19 +2,22 @@ import java.util.Stack;
 
 class Solution {
     boolean solution(String s) {
-        Stack<Character> stack = new Stack<>();
-        
+                Stack<Character> stack = new Stack<>();
+
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
 
-            if (stack.isEmpty() && ch == ')') {
+            // 첫 번째 괄호가 닫는 괄호인 경우에는 성립 불가능
+            if (i == 0 && ch == ')') {
                 return false;
             }
 
-            if (ch == '(') {
+            if (ch == ')') {
+                if(!stack.isEmpty() && stack.peek() == '(') {
+                    stack.pop();
+                }
+            } else {
                 stack.push(ch);
-            } else if (ch == ')' && stack.peek() == '(') {
-                stack.pop();
             }
         }
 
