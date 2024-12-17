9124736beb331835322510919a39df351c0b7610
dongmin <ddmkim94@gmail.com>
2024-12-17 12:04:59 +0900

    Time: 5 ms (72.38%), Space: 43.8 MB (83.49%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        String str = String.valueOf(x);
+
+        int left = 0;
+        int right = str.length() - 1;
+
+        while(left < right) {
+
+            if(str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
